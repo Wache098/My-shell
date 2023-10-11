ddc572964faebb3f00d571b4f4d93f99c6236f3b
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-11 07:08:17 +0300

    Initial commit

---
+++ shell.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main() {
+    char *username = "jay"; 
+    char *hostname = "localman";
+    char cwd[1024];
+
+    while (1) {
+        // check if in interactive mode 
+        if (isatty(0)) { 
+            //  Display prompt
+            getcwd(cwd, sizeof(cwd));
+            printf("%s@%s:%s$ ", username, hostname, cwd);
+            fflush(stdout); 
+        }
+
+        // Read user input
+        char input[1024];
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;
+        }
+
+        // Example: Exit the loop if the user enters "exit"
+        if (strcmp(input, "exit\n") == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
