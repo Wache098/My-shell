00d662a462d3cbf442c84913f909a87f5e4ea2e0
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 13:28:05 +0300

    Handling of EOF

--- getline.c
+++ getline.c
@@ -9,11 +9,18 @@ lineptr stores the address of the created buffer
 char* read_line(void){
     size_t len = 0;
     /*char *buf = malloc(sizeof(char) * n );*/
+
     char *line = NULL;
-    getline(&line, &len, stdin);
+   if( getline(&line, &len, stdin)){
+    if(feof(stdin)){
+        exit(EXIT_SUCCESS); /*Got an EOF*/
+    }else {
+        perror("readline");
+        exit(EXIT_FAILURE);
+    }
+   }
 
-    free(line);
-
-
+    return(line);
+    /*free(line);*/
     /*return 0;*/
 }
