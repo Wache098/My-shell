f32d72f31bab16e70a2779b6cd30811b810be9ae
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 21:37:48 +0300

    Handle parsing without using "strtok"

--- get_line.c
+++ get_line.c
@@ -23,28 +23,29 @@ char *get_line(void){
                 }
                 break; /*Return the last line*/
             }
-    }
+        }
+        /*Allocate memory for the line*/
+        len += chars_left;
+        line = realloc(line, len + 1);
+        if(!line){
+            perror("get_line");
+            exit(EXIT_FAILURE);
+        }
 
-    len += chars_left;
-    line = realloc(line, len + 1);
-    if(!line){
-        perror("get_line");
-        exit(EXIT_FAILURE);
+        /*Find the end of such line*/
+        if(newline){
+            size_t line_len = newline -ptr;
+            strncpy(line + len - chars_left, ptr, line_len);
+            line[len - chars_left + line_len] = '\0';
+            ptr = newline + 1;
+            chars_left -= line_len + 1;
+            return line;
+        }
+        
+        /*Copy buffer to the line and continue*/
+        strncpy(line + len -chars_left, ptr, chars_left);
+        chars_left = 0;
     }
-
-    if(newline){
-        size_t line_len = newline -ptr;
-        strncpy(line + len - chars_left, ptr, line_len);
-        line[len - chars_left + line_len] = '\0';
-        ptr = newline + 1;
-        chars_left -= line_len + 1;
-        return line;
-    }
-    
-    strncpy(line + len -chars_left, ptr, chars_left);
-    chars_left = 0;
-    }
-
     line[len] = '\0';
     return line;
 }
--- parser.c
+++ parser.c
@@ -1,41 +1,42 @@
 #include "shell.h"
-/*Split line into arguments using strtok()*/
 
 char **parse_line(char *line){
-    int bufsize = 64, position = 0;
+    size_t bufsize = 64, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
-    char *token, *saveptr;
-    token = 0;
+    char *token_start = NULL;
+    size_t len = strlen(line);
+    int in_word = 0;
+
 
     if(!tokens){
-        /*  fprintf(stderr, "simple_shell: allocation error\n");*/
-        perror("ERROR");
-        /*return(NULL);*/
+        fprintf(stderr, "simple_shell: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    /*Handling of commentss*/
-    char *comment_pos = strchr(line, "#");
-    if(comment_pos){
-        *comment_pos = "\0";
+    for(size_t i = 0; i < len; i++){
+        char c= line[i];
 
-    }
-    
-    token = strtok(line, "\t\r\n\a");
-    while(token != NULL){
-        tokens[position] = token;
-        position++;
-
-        if(position >= bufsize){
-            bufsize += 64; 
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if(!tokens){
-                perror("ERROR");
-                exit(EXIT_FAILURE);
-                /*return (NULL);*/
+        if(c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\a' || i == len -1){
+            if(in_word){
+                size_t token_len = line + i -token_start + (i == len -1);
+                token_start[token_len - ( i == len -1)] = '\0';
+                tokens[position] = strdup(token_start);
+                position++;
+                if(position >= bufsize){
+                    bufsize += 64;
+                    tokens = realloc(tokens, bufsize * sizeof(char*));
+                    if(!tokens){
+                        fprintf(stderr, "simple_shell: allocation error\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                in_word = 0;
             }
+        }else if(!in_word){
+            /*token_start = &line[i];*/
+            token_start = line + 1;
+            in_word = 1;
         }
-        token = strtok(NULL, " \t\r\n\a");
     }
     tokens[position] = NULL;
     return tokens;
