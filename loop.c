9c4384aaf69611d1d814b8d769047b4b33ce97af
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 20:59:47 +0300

    Updated tokenization of the input line into an array

--- loop.c
+++ loop.c
@@ -2,14 +2,17 @@
 
 void loop(void){
     char *line;
+    char **args;
     int status = 1;
 
     while(status){
         printf("$ ");
 
         line = read_line();
+        args = parse_line(line);
         status = execute_command(line);
+
         free(line);
+        free(args);
     }
 }
-
