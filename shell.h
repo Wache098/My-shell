95b6ddfe9f22440faa0327f525f2f255f14a2e73
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 12:57:31 +0300

    Display prompt

--- shell.h
+++ shell.h
@@ -1,11 +1,16 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#endif /*SHELL_H*/
+#define MAX_INPUT_SIZE 1024
+
+
+
+#endif /*MAIN_H*/
