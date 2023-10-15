1d7f22ff9599c570044dfe5fce4d2908677f4f82
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 19:19:48 +0300

    Handle comments (#)

--- parser.c
+++ parser.c
@@ -4,16 +4,23 @@
 char **parse_line(char *line){
     int bufsize = 64, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
+    char *token, *saveptr;
     token = 0;
 
-    if(!token){
+    if(!tokens){
         /*  fprintf(stderr, "simple_shell: allocation error\n");*/
         perror("ERROR");
         /*return(NULL);*/
         exit(EXIT_FAILURE);
     }
 
+    /*Handling of commentss*/
+    char *comment_pos = strchr(line, "#");
+    if(comment_pos){
+        *comment_pos = "\0";
+
+    }
+    
     token = strtok(line, "\t\r\n\a");
     while(token != NULL){
         tokens[position] = token;
