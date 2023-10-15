320d3d738eb887ba9ba8a90ddd9cb562602e197f
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-15 21:54:32 +0300

    Handled commands separator

--- loop.c
+++ loop.c
@@ -3,6 +3,7 @@
 void loop(void){
     char *line;
     char **args;
+    char **commands;
     int status = 1;
 
     while(status){
@@ -10,9 +11,17 @@ void loop(void){
 
         line = read_line();
         args = parse_line(line);
+        commands = parse_line(line);
         status = execute_command(line);
 
+        for(size_t i = 0; commands[i] != NULL; i++ ){
+            char **args = parse_line(commands[i]);
+            status = execute_command(args);
+            free(args);
+            free(commands[i]);
+        }
+
         free(line);
-        free(args);
+        free(commands);
     }
 }
