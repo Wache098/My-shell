cdf4636e9916b10ee40547934e27a65a80f6875b
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-16 10:04:43 +0300

    Removed syntax errors

--- loop_file.c
+++ loop_file.c
@@ -6,14 +6,15 @@ void loop_file(FILE *fp){
     ssize_t nread;
     char **commands;
     int status = 1;
+    size_t i;
 
     while((nread = getline(&line, &len, fp)) != -1 && status){
         if(line[nread -1] == '\n'){
-            line[--nread] = '\0'; // remove the newline character at end of string
+            line[--nread] = '\0'; /*remove the newline character at end of string*/
         }
         commands= parse_line(line);
 
-        for(size_t i = 0; commands[i] != NULL && status; i++){
+        for(i = 0; commands[i] != NULL && status; i++){
             char **args = parse_line(commands[i]);
             status = execute_command(args);
             free(args);
