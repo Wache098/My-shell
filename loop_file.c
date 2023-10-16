0d2d87f2c9f4d8bfda8938972c106cb2428058a6
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-16 09:53:13 +0300

    Syntax error update

---
+++ loop_file.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+void loop_file(FILE *fp){
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char **commands;
+    int status = 1;
+
+    while((nread = getline(&line, &len, fp)) != -1 && status){
+        if(line[nread -1] == '\n'){
+            line[--nread] = '\0'; // remove the newline character at end of string
+        }
+        commands= parse_line(line);
+
+        for(size_t i = 0; commands[i] != NULL && status; i++){
+            char **args = parse_line(commands[i]);
+            status = execute_command(args);
+            free(args);
+            free(commands[i]);
+        }
+        free(commands);
+    }
+    free(line);
+}
