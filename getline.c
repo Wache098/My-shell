255f8fda2bf9d4e932edde733509f38512b87565
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-17 06:16:21 +0300

    Store  return value of the execute_command

--- getline.c
+++ getline.c
@@ -11,15 +11,15 @@ char* read_line(void){
     /*char *buf = malloc(sizeof(char) * n );*/
 
     char *line = NULL;
-   if( getline(&line, &len, stdin)){
+    if( getline(&line, &len, stdin)){
     if(feof(stdin)){
         exit(EXIT_SUCCESS); /*Got an EOF*/
     }else {
-        perror("readline");
+        free(line); /* avoid memory leaks when getline fails */
+        perror("error while reading the line from stdin");
         exit(EXIT_FAILURE);
     }
-   }
-
+    }
     return(line);
     /*free(line);*/
     /*return 0;*/
