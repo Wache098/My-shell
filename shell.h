793e7ddb22016206d625f45e2aa32e547abf124c
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-17 06:12:43 +0300

    Defined macro to access nonstandard extension functions

--- shell.h
+++ shell.h
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -5,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stddef.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
--- vars.c
+++ vars.c
@@ -9,7 +9,7 @@ char *replace_vars(char *arg, int last_status){
         asprintf(&new_arg,"%.*s%d%s",(int)(var -arg), arg, last_status, var +2);
         return new_arg;
     }else if((var = strstr(arg, "$$")) != NULL){
-        asprintf(&new_arg,"%.*s%ld%s", (int)(var-arg), arg, getpid(), var + 2);
+        asprintf(&new_arg,"%.*s%d%s", (int)(var-arg), arg, getpid(), var + 2);
         return new_arg;
     }
     return strdup(arg);
