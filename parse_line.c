9590f96b92dea16d115fbd2ea5e52d4bf4f5596f
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-17 06:09:32 +0300

    Debugging of the null termination

--- parse_line.c
+++ parse_line.c
@@ -1,45 +1,48 @@
 #include "shell.h"
 
-char **parse_line(char *line){
+char **parse_line(char *line) {
     size_t bufsize = 64, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
-    char *token_start = NULL;
-    size_t len = strlen(line);
-    int in_word = 0;
-    /*int in_command = 1;*/
-    size_t i;
+    char *token, *saveptr;
+    char *comment_pos;
+        int i;
 
 
-    if(!tokens){
+    if (!tokens) {
         fprintf(stderr, "simple_shell: allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    for(i = 0; i < len; i++){
-        char c= line[i];
+    /*Handle comments: ignore everything after #*/
+    comment_pos = strchr(line, '#');
+    if (comment_pos) {
+        *comment_pos = '\0';
+    }
 
-        if(c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\a' || i == len -1){
-            if(in_word){
-                size_t token_len = line + i -token_start + (i == len -1);
-                token_start[token_len - ( i == len -1)] = '\0';
-                tokens[position] = strdup(token_start);
-                position++;
-                if(position >= bufsize){
-                    bufsize += 64;
-                    tokens = realloc(tokens, bufsize * sizeof(char*));
-                    if(!tokens){
-                        fprintf(stderr, "simple_shell: allocation error\n");
-                        exit(EXIT_FAILURE);
-                    }
-                }
-                in_word = 0;
+    token = strtok_r(line, " \t\r\n\a", &saveptr);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += 64;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "simple_shell: allocation error\n");
+                exit(EXIT_FAILURE);
             }
-        }else if(!in_word){
-            /*token_start = &line[i];*/
-            token_start = line + 1;
-            in_word = 1;
         }
+
+         token = strtok_r(NULL, " \t\r\n\a", &saveptr);
+
+        
     }
     tokens[position] = NULL;
+    /**
+     *   for (i = 0; tokens[i] != NULL; i++) {
+        printf("token[%d]: %s\n", i, tokens[i]);
+    }
+    return tokens;
+    */  
     return tokens;
 }
